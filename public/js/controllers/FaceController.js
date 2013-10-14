693b08d70b4a80aa8d907711d14bc8c901327676
mateusz <mateusz@hp.(none)>
2013-10-14 19:36:12 +0200

    next logic

--- public/js/controllers/FaceController.js
+++ public/js/controllers/FaceController.js
@@ -4,18 +4,24 @@ define(['_'], function(_) {
 		Faces.getRandom().success(function(face) {
 			$scope.faceUrl = 'images/faces/' + face.photo;
 			$scope.face = face;
-  		});		
+  		});
+  		$scope.buttonText = 'Check!';
 	}
 
 	var FaceController = function($scope, Faces) {
 
 		init($scope, Faces);
 
-  		$scope.checkName = function() {
-			Faces.checkName($scope.face, $scope.guess).success(function(resp) {
-				$scope.result = resp.result;
-				$scope.message = resp.message;
-	  		});
+  		$scope.submit = function() {
+  			if($scope.buttonText === 'Next') {
+  				init($scope, Faces);
+  			} else {
+				Faces.checkName($scope.face, $scope.guess).success(function(resp) {
+					$scope.result = resp.result;
+					$scope.message = resp.message;
+					$scope.buttonText = 'Next';
+		  		});
+  			}
   		}
 
 	};
