c4d37bc18667f4a280bda3b96cabef334e709ae0
mateusz <mateusz@hp.(none)>
2013-10-14 21:53:03 +0200

    initial database content

---
+++ db_init.js
@@ -0,0 +1,2 @@
+db.faces.insert({ "forename" : "Maka", "surname" : "Paka", "photos" : [  "maka-paka.jpg" ]});
+db.faces.insert({ "forename" : "Igi", "surname" : "Pigiel", "photos" : [  "igi-pigiel.jpg" ]});
