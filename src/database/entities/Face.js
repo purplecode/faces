489497ff07fc5a40dafc75f642e6d4ec34108f4a
Tomusdrw <tomusdrw@gmail.com>
2013-10-22 22:01:59 +0200

    Model with additional fields.

--- src/database/entities/Face.js
+++ src/database/entities/Face.js
@@ -5,7 +5,13 @@ var faceSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
     forename: String,
     surname: String,
-    photos: Array
+    photos: Array,
+    title: String,
+    phoneNumber: String,
+    skills: String,
+    projects: String,
+    website: String,
+    about: String
 });
 
 module.exports = mongoose.model('Face', faceSchema);
